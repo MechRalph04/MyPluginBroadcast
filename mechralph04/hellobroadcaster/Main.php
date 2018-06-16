3a832bfb560f77df6b89db20fe9c840a908bfebb
MechRalph04 <roxasraphael336@gmail.com>
2018-06-17 01:34:56 +0800

    Update Main.php

--- mechralph04/hellobroadcaster/Main.php
+++ mechralph04/hellobroadcaster/Main.php
@@ -3,11 +3,12 @@ namespace mechralph04\hellobroadcaster
 use pocketmine\plugin\PluginBase;
 use pocketmine\command\CommandSender;
 use pocketmine\command\Command;
+use pocketmine\Player;
 class MyPlugin extends PluginBase{
   public function onCommand(CommandSender $sender, Command $command, $label, array $args){
     $commandName = $command->getName();
     if($commandName === "hbw"){
-      $sender->sendMessage("Hi!");
+      $sender->sendMessage("A hi from", $sender->getName());
       return true;
     }
     return false;
