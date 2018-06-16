6fda0c015b80bb485d5ae353771c86184801da37
MechRalph04 <roxasraphael336@gmail.com>
2018-06-17 01:37:21 +0800

    Update Main.php

--- mechralph04/hellobroadcaster/Main.php
+++ mechralph04/hellobroadcaster/Main.php
@@ -5,6 +5,9 @@ use pocketmine\command\CommandSender;
 use pocketmine\command\Command;
 use pocketmine\Player;
 class MyPlugin extends PluginBase{
+  public function onEnable(){
+    $this->getLogger()->info("Hello Broadcaster enabled");
+  }
   public function onCommand(CommandSender $sender, Command $command, $label, array $args){
     $commandName = $command->getName();
     if($commandName === "hbw"){
