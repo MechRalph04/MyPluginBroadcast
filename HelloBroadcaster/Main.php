46e43072592f07ff24c2ba4ec7be5d77e097db43
MechRalph04 <roxasraphael336@gmail.com>
2018-06-17 09:27:48 +0800

    Update and rename mechralph04/hellobroadcaster/Main.php to HelloBroadcaster/Main.php

--- HelloBroadcaster/Main.php
+++ HelloBroadcaster/Main.php
@@ -1,5 +1,5 @@
 <?php
-namespace mechralph04\hellobroadcaster
+namespace HelloBroadcaster;
 use pocketmine\plugin\PluginBase;
 use pocketmine\command\CommandSender;
 use pocketmine\command\Command;
@@ -11,7 +11,7 @@ class MyPlugin extends PluginBase{
   public function onCommand(CommandSender $sender, Command $command, $label, array $args){
     $commandName = $command->getName();
     if($commandName === "hbw"){
-      $sender->sendMessage("A hi from", $sender->getName());
+      $sender->sendMessage("Happy Father's Day from", $sender->getName());
       return true;
     }
     return false;
